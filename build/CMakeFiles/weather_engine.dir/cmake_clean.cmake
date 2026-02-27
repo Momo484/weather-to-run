e11c838eca5544544585427d0fcdda5d1945869c
Aabhushan Shrestha <z5590070@ad.unsw.edu.au>
2026-02-27 14:21:03 +1100

    MVP finished for backend

---
+++ build/CMakeFiles/weather_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weather_engine.dir/WeatherEngine.cpp.o"
+  "CMakeFiles/weather_engine.dir/WeatherEngine.cpp.o.d"
+  "CMakeFiles/weather_engine.dir/main.cpp.o"
+  "CMakeFiles/weather_engine.dir/main.cpp.o.d"
+  "weather_engine"
+  "weather_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weather_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
